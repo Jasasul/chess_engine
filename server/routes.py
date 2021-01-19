603247e50c3b3388dbdb058dc99ebe36b44d5b07
Jasasul <jsulman8@gmail.com>
2021-01-19 14:07:46 +0100

    server-client comms added and gui added

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for, redirect, Blueprint
+from flask import render_template, url_for, redirect, Blueprint, jsonify
+import random as rn
 
 routes = Blueprint('routes', __name__)
 
@@ -6,3 +7,8 @@ routes = Blueprint('routes', __name__)
 @routes.route('/home')
 def home():
     return render_template('home.html')
+
+@routes.route('/handle_request', methods=['POST'])
+def handle_request():
+    fen = 'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2'
+    return jsonify(fen=fen)
